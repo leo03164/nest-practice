e0934ab8e0efd5df4d840599d58a537cbdb8cdbd
leo0316 <leo.chen@wizigo.tw>
2021-12-13 13:33:09 +0800

    task project

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,50 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { TaskStatus } from "./tasks.status.enum";
+import { CreateTaskDto } from './dto/create-task-dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TasksRepository } from './tasks.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Task } from './task.entity';
+@Injectable()
+export class TasksService {
+  constructor(
+    @InjectRepository(TasksRepository)
+    private taskRepository: TasksRepository
+  ){}
+
+  // private tasks: Task[] = [];
+
+  // getAllTasks(): Task[]{
+  //   return this.tasks;
+  // }
+
+  createTask(createTaskDto: CreateTaskDto): Promise<Task>{
+    return this.taskRepository.createTask(createTaskDto);
+  }
+
+  async getTaskById(id: string): Promise<Task>{
+    const found = await this.taskRepository.findOne(id);
+    if(!found){
+      throw new NotFoundException(`Task Id not found`);
+    }
+    return found;
+  }
+
+
+  deleteTaskById(id: string): void {
+    this.taskRepository.deleteTask(id);
+  }
+
+  async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
+    const task = await this.getTaskById(id);
+    
+    task.status = status;
+
+    await this.taskRepository.save(task);
+    return task;
+  }
+
+  getTasks(filterDto: GetTasksFilterDto): Promise<Task[]>{
+    return this.taskRepository.getTasks(filterDto);
+  }
+}
